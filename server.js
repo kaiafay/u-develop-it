4e0647e649a805d627b1b31442b75af43566f924
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-09 19:53:07 -0700

    connect to the mysql database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// import mysql2 package
+const mysql = require('mysql2');
 // PORT designation
 const PORT = process.env.PORT || 3001;
 // app expression
@@ -6,6 +8,19 @@ const app = express();
 // express.js middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql username
+        user: 'root',
+        // mysql password
+        password: 'codenow18',
+        database: 'election'
+    },
+    console.log("Connected to the election database.")
+);
 
 // default response for any other request (not found)
 // make sure this is the last route
