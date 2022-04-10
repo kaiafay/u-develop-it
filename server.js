02458ee09a18d3e198441c1464c9ad0cefcf2269
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-09 20:27:00 -0700

    create api endpoint to retrieve all candidates

--- server.js
+++ server.js
@@ -22,45 +22,60 @@ const db = mysql.createConnection(
     console.log("Connected to the election database.")
 );
 
-// returns all data from the candidates table
-// the query method executes the callback with all the resulting rows that match the query
-// the callback function captures the responses from the query
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// returns all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    // the query method executes the callback with all the resulting rows that match the query
+    // the callback function captures the responses from the query
+    db.query(sql, (err, rows) => {
+        if(err) {
+            // if error, return status 500 (server error) with the error's message
+            res.status(500).json({ error: err.message });
+            // exit the database if error is encountered
+            return;
+        }
+        // if success, return response data with a 'success' message
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
 });
 
-// returns a single candidate based off id
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(row);
-});
 
-// deletes a candidate
-// the question mark denotes a placeholder and makes this a prepared statement
-// a prepared statement can execute repeatedly using different values
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(result);
-});
+// // returns a single candidate based off id
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
 
-// saves the sql command as a variable
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-             VALUES (?,?,?,?)`;
+// // deletes a candidate
+// // the question mark denotes a placeholder and makes this a prepared statement
+// // a prepared statement can execute repeatedly using different values
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
-// saves the sql parameters to a variable
-const params = [1, 'Ronald', 'Firbank', 1];
+// // saves the sql command as a variable
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?,?,?,?)`;
 
-// creates a candidate
-db.query(sql, params, (err, result) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(result);
-});
+// // saves the sql parameters to a variable
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// // creates a candidate
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // default response for any other request (not found)
 // make sure this is the last route
