bc43b63ddd4878f691a79c212121989e1b245971
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-10 20:02:14 -0700

    create api endpoint to retrieve a single party

--- server.js
+++ server.js
@@ -91,6 +91,23 @@ app.get('/api/parties', (req, res) => {
     });
 });
 
+// returns a single party based off id
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
 // deletes a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     // the question mark denotes a placeholder and makes this a prepared statement
